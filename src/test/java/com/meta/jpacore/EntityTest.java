c26507578d87abe266f42a468d97796315bf0b09
yunminjae03 <dbsaslwo012@g.hongik.ac.kr>
2026-01-20 15:39:22 +0900

    test : JPA의 동작 방식 확인을 위한 설정

---
+++ src/test/java/com/meta/jpacore/EntityTest.java
@@ -0,0 +1,25 @@
+package com.meta.jpacore;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EntityTest {
+  EntityManagerFactory emf;
+  EntityManager em;
+
+  @BeforeEach
+  void setUp() {
+    emf = Persistence.createEntityManagerFactory("memo");
+    em = emf.createEntityManager();
+  }
+
+  @Test
+  void test1(){
+
+  }
+}
